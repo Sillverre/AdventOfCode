1ac3cf6581fe27f40d145a88b859c8cec64492ac
Edgard Pinget <edgardpinget@MacBook-Pro-de-Edgard.local>
2021-12-05 14:15:46 +0100

    feat: add day 4 + 5

--- 2021/4/resolve4.py
+++ 2021/4/resolve4.py
@@ -1,11 +1,146 @@
+import copy
+
 f = open("input.txt", "r")
 
+def transform_data(file):
+    # Get incoming inputs
+    incoming = f.readline().split(',')
+    incoming = list(map(int, incoming))
+    
+
+    # Get all boards
+    boards = []
+    marked_boards = []
+    lines = f.readlines()
+
+    count = 0
+    board = []
+    marked_board = []
+
+    for line in lines:
+
+        if count != 0:
+            numeric_line = line.split()
+            numeric_line = list(map(int, numeric_line))
+            board.append(numeric_line)
+            marked_board.append([0]*len(numeric_line))
+            if count == 5:
+                count = -1
+                boards.append(board)
+                marked_boards.append(marked_board)
+                board = []
+                marked_board = []
+        count +=1
+    
+    return incoming, boards, marked_boards
+        
+def isWinner(marked_board):
+    # Row Search:
+    for i in range(len(marked_board[0])):
+        answer = True
+        for line in marked_board:
+            # Line Search:
+            if sum(line) == 5:
+                return True
+
+            if line[i] != 1:
+                answer = False
+
+        if answer == True:
+            return True
+
+    return False
+
+def isInBoard(number, board):
+    position = [0,0]
+    i = 0
+    for line in board:
+        if number in line:
+            j = line.index(number)
+            position = [i,j]
+            return True, position
+        i += 1
+    return False , position
+
+def sumUnmarked(marked_board, board):
+    summ = 0
+    for i in range(len(marked_board)):
+        for j in range(len(marked_board[0])):
+            if marked_board[i][j] == 0:
+                summ += board[i][j]
+    return summ
+
+
+
 def part1(arg):
+    incoming, boards, marked_boards = transform_data(arg)
+
+    for number in incoming :
+        for board in boards:
+            isIn, position = isInBoard(number, board)
+            i = boards.index(board)
+            if isIn:
+                marked_boards[i][position[0]][position[1]] = 1
+
+            if isWinner(marked_boards[i]):
+                sumRemain = sumUnmarked(marked_boards[i], board)
+                result = sumRemain * number
+                return result
  
 
 def part2(arg):
+    incoming, boards, marked_boards = transform_data(arg)
+
+    for number in incoming :
+        for board in boards:
+            isIn, position = isInBoard(number, board)
+            i = boards.index(board)
+            if isIn:
+                marked_boards[i][position[0]][position[1]] = 1
+
+            if isWinner(marked_boards[i]):
+                del boards[i]
+                del marked_boards[i]
+
+            if len(boards) == 1:
+                isIn, position = isInBoard(number, boards[0])
+                if isIn:
+                    marked_boards[0][position[0]][position[1]] = 1
+
+                print(marked_boards[0],boards[0])
+                sumRemain = sumUnmarked(marked_boards[0], boards[0])
+                print(sumRemain, number)
+                result = sumRemain * number
+                return result
+
+def part2_alternative(arg):
+    incoming, boards, marked_boards = transform_data(arg)
+
+    last = 0
+    summToRemind = 0
+
+    for board in boards:
+        marked_board = copy.deepcopy(marked_boards[0])
+        i = 0
+        for number in incoming: 
+            isIn, position = isInBoard(number, board)
+            if isIn:
+                marked_board[position[0]][position[1]] = 1
+
+            if isWinner(marked_board):
+                sumRemain = sumUnmarked(marked_board, board)
+                if i > last:
+                    summToRemind = sumRemain
+                    last = i
+                    boardToRemind = board
+                break
+
+            i += 1
+    return incoming[last] * summToRemind
+            
+    
  
 
 ## Select function here :
-part1(f)
+print(part2_alternative(f))
 
--- 2021/5/resolve5.py
+++ 2021/5/resolve5.py
@@ -1,11 +1,163 @@
+import copy 
+
 f = open("input.txt", "r")
 
+def transform_data(arg):
+    start_points = []
+    end_points = []
+
+    lines = f.readlines()
+
+    for line in lines:
+        start, arrow, end = line.split()
+        start_x, start_y = start.split(',')
+        end_x, end_y = end.split(',')
+        start_points.append([int(start_x), int(start_y)])
+        end_points.append([int(end_x), int(end_y)])
+
+    return start_points, end_points
+
+def find_max_array(start_points, end_points):
+    all_x = []
+    all_y = []
+    for point in start_points:
+        all_x.append(point[0])
+        all_y.append(point[1])
+    for point in end_points:
+        all_x.append(point[0])
+        all_y.append(point[1])
+
+    max_x = max(all_x) + 1
+    max_y = max(all_y) + 1
+
+    diagram = [[0 for x in range(max_x)] for y in range(max_y)] 
+
+    return diagram
+
+def check_vertical(start_point, end_point):
+
+    if start_point[0] == end_point[0]:
+        return True, start_point[0], start_point[1], end_point[1]
+    else:
+        return False, 0, 0, 0
+
+def check_horizontal(start_point, end_point):
+
+    if start_point[1] == end_point[1]:
+        return True, start_point[1], start_point[0], end_point[0]
+    else:
+        return False, 0, 0, 0
+
+def check_diagonal(start_point, end_point):
+    # Diagonal Type 1: Top left -> bot right
+    if (start_point[0] - end_point[0])  == (start_point[1] - end_point[1]):
+        return True, 0
+    # Diagonal Type 2: Top right -> bot left
+    elif (start_point[0] - end_point[0]) == (end_point[1] - start_point[1]):
+        return True, 1
+
+    return False, 0
+    
+
+def write_vertical(diagram, y, x1, x2):
+    if x1 <= x2:
+        for x in range(x1, x2 +1):
+            diagram[x][y] += 1
+    else:
+        for x in range(x2, x1 +1):
+            diagram[x][y] += 1
+
+    return diagram
+
+def write_horizontal(diagram,  x, y1, y2):
+
+    if y1 <= y2:
+        for y in range(y1, y2 +1):
+            diagram[x][y] += 1
+    else:
+        for y in range(y2, y1 +1):
+            diagram[x][y] += 1
+    
+    return diagram
+
+def write_diagonal(diagram,  type, start_point, end_point):
+    if type == 0 :
+        if start_point[0] < end_point[0]:
+            for i in range(end_point[0] - start_point[0] + 1):
+                diagram[start_point[1] + i][start_point[0] + i] += 1
+        else:
+            for i in range(start_point[0] - end_point[0] + 1):
+                diagram[end_point[1] + i][end_point[0] + i] += 1
+    if type == 1 :
+        if start_point[0] < end_point[0]:
+            for i in range(end_point[0] - start_point[0] + 1):
+                diagram[start_point[1] - i][start_point[0] + i] += 1
+            
+        else:
+            for i in range(start_point[0] - end_point[0] + 1):
+                diagram[start_point[1] + i][start_point[0] - i] += 1
+
+    return diagram
+        
+    
+def count_diagram(diagram):
+    count = 0
+    for x in range(len(diagram)):
+        for y in range(len(diagram[0])):
+            if diagram[x][y] >1 :
+                count += 1
+    return count
+
+def diagram_matrix(diagram):
+    for line in diagram:
+        print(line)
+
 def part1(arg):
- 
+    start_points, end_points = transform_data(arg)
+    diagram = find_max_array(start_points, end_points)
+
+    for i in range(len(start_points)):
+
+        check, x, y1, y2 = check_vertical(start_points[i], end_points[i])
+        if check:
+            print("vertical")
+            diagram = write_vertical(diagram, x, y1, y2)
+
+        check, y, x1, x2 = check_horizontal(start_points[i], end_points[i])
+        if check:
+            print("horizontal")
+            diagram = write_horizontal(diagram, y, x1, x2)
+
+    print(diagram)
+    return count_diagram(diagram) 
+
+
 
 def part2(arg):
+    start_points, end_points = transform_data(arg)
+    diagram = find_max_array(start_points, end_points)
+
+    for i in range(len(start_points)):
+
+        check, x, y1, y2 = check_vertical(start_points[i], end_points[i])
+        if check:
+            print("vertical")
+            diagram = write_vertical(diagram, x, y1, y2)
+
+        check, y, x1, x2 = check_horizontal(start_points[i], end_points[i])
+        if check:
+            print("horizontal")
+            diagram = write_horizontal(diagram, y, x1, x2)
+
+        check, type = check_diagonal(start_points[i], end_points[i])
+        if check:
+            print("diagonal ", type , start_points[i], end_points[i])
+            diagram = write_diagonal(diagram, type, start_points[i], end_points[i])
+    
+    
+    return count_diagram(diagram) 
  
 
 ## Select function here :
-part1(f)
+print(part2(f))
 
